c704a64faec9d6fc33fcb0b24786a454c800a615
upupdayday <sunhanqing1987@163.com>
2018-11-27 04:56:03 +0800

    创建button group

---
+++ src/button-group.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="g-button-group"><!---slot不能作为根节点--->
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "button-group"
+    }
+</script>
+
+<style lang="scss">
+    .g-button-group{
+        display: inline-flex;
+        vertical-align: middle;
+        >.g-button{
+            border-radius: 0;
+            &:not(:first-child){
+                margin-left: -1px;
+            }
+            &:first-child{
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child{
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+        }
+
+
+
+    }
+</style>
