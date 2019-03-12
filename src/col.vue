b998c8fe838a2901684b7d941d47985e0d818cff
upupdayday <sunhanqing1987@163.com>
2019-03-13 05:59:43 +0800

    支持空白段

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
+    <div class="col" :class="[`col-${span}`, offset &&`offset-${offset}` ]">
         <slot></slot>
     </div>
 </template>
@@ -8,6 +8,9 @@
         name: "gCol",
         props:{
             span:{
+                type:[Number,String]
+            },
+            offset:{
                 type:[Number,String]
             }
         }
@@ -24,6 +27,13 @@
         @for $n from 1 through 24{
             &.#{$class-prefix}#{$n} {
                 width: ($n/24)*100%;
+            }
+        }
+
+        $class-prefix: offset-;
+        @for $n from 1 through 24{
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n/24)*100%;
             }
         }
     }
