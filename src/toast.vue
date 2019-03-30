2f262fd5073ed8d7a810e706b977caa30fc35a1b
upupdayday <sunhanqing1987@163.com>
2019-03-31 00:06:21 +0800

    添加toast和plugin

---
+++ src/toast.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="toast">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "gToast"
+    }
+</script>
+
+<style lang="scss" scoped>
+    $font-size: 14px;
+    $toast-height: 40px;
+    $toast-bg: rgba(0,0,0,0.75);
+    .toast{
+        font-size:$font-size; height: $toast-height; line-height: 1.8;
+        position: fixed; top:0; left:50%; transform: translateX(-50%);
+        display: flex; align-items: center;
+        box-shadow: 0 0 3px 0 rgba(0,0,0,0.50);
+        border-radius: 4px;
+        padding: 0 16px;
+        background: $toast-bg;
+        color: white;
+    }
+</style>
